8144390735592844e940e18f23c763515d55fc5d
unknown <leega0711@naver.com>
2017-09-14 20:11:15 +0900

    Initial commit

---
+++ app/src/main/java/firebasedbexample/leega0711/emirim/hs/kr/a20170914and/MainActivity.java
@@ -0,0 +1,13 @@
+package firebasedbexample.leega0711.emirim.hs.kr.a20170914and;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
